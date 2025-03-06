35e4f4cc22ca5195775804fd26cb02ffe7bab6f7
JavaRaptor <nolanbiquet08@gmail.com>
2025-03-06 18:03:10 +0100

    del files

--- events/ready.js
+++
@@ -1,5 +0,0 @@
-module.exports = (client) => {
-  console.log(`${client.user.username} Log activate`)
-  client.user.setActivity(`${client.guilds.cache.size} Servers`)
-  client.user.setStatus('idle')
-}
